e584297fa7f1f8eb8b9c3ea858e88b89748861e8
28521323 <quintin.mare@gmail.com>
2020-09-27 18:06:25 +0200

    Removed unnecessary objects

--- BlazorSignalRApp/BlazorSignalRApp/Server/Controllers/WeatherForecastController.cs
+++ BlazorSignalRApp/BlazorSignalRApp/Server/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
-﻿using BlazorSignalRApp.Shared;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,17 +23,8 @@ namespace BlazorSignalRApp.Server.Controllers
             this.logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
+        
+    
+
